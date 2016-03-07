b4a0e95990e57952aa849d3319016c0d3735b3f7
fino87 <fino87@mail.ru>
2016-03-07 12:43:35 +0300

    ver 0.3

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo rm /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
 sudo /etc/init.d/nginx restart
