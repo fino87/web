c7d4cab701c6e54ae33c32417ca2690a4f4a1e69
fino87 <fino87@mail.ru>
2016-03-09 20:47:27 +0300

    Ver 0.5

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 sudo rm /etc/nginx/sites-available/default
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
+gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
