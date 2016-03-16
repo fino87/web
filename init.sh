dde3609e27cece88028adae692a82568adb82bf6
fino87 <fino87@mail.ru>
2016-03-16 21:09:18 +0300

    ver 0.6

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
+sudo gunicorn --bind 0.0.0.0:8000 --access-logfile acc.log --error-logfile err.log ask.wsgi:application
