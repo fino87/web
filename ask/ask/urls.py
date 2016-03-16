dbd8358eb939830e12718ebcc45a0d2ab003733b
fino87 <fino87@mail.ru>
2016-03-16 21:00:40 +0300

    ver 0.6

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,16 +1,9 @@
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('qa.views',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^login/', include(login.site.urls)),
-    url(r'^signup/', include(signup.site.urls)),
-    url(r'^question/\d+', 'test', name='question',)
-    url(r'^ask/', include(ask.site.urls)),
-    url(r'^popular/', include(popular.site.urls)),
-    url(r'^new/', include(new.site.urls))
-)
+urlpatterns = patterns('qa.views',                                              
+   url(r'^$', 'test'),                                                              
+   url(r'^login/.*$', 'test', name='login'),                                    
+   url(r'^signup/.*', 'test', name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
+   url(r'^ask/.*', 'test', name='ask'),                                         
+   url(r'^popular/.*', 'test', name='popular'),                                 
+   url(r'^new/.*', 'test', name='new'),                                         
+)    
