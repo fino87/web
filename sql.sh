64b66ed126f6e25e3f01cadc346ca3f3afb4679d
fino87 <fino87@mail.ru>
2016-03-16 23:51:39 +0300

    ver 0.7

--- sql.sh
+++ sql.sh
@@ -1,3 +1,3 @@
 mysql -uroot -e "create database db1"
 mysql -uroot -e "create user 'django'@'localhost' identified by '123'"
-mysql -uroot -e "grant all on webdb.* to 'django'@'localhost'"
+mysql -uroot -e "grant all on db1.* to 'django'@'localhost'"
