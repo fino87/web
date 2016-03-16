2d445f0533d2699ff6a2e79861cd4ccd6a641e07
fino87 <fino87@mail.ru>
2016-03-16 23:27:47 +0300

    ver 0.6

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -56,10 +56,11 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webdb',
+	'USER': 'fino',
+	'PASSWORD': '123',
 }
 
 # Internationalization
