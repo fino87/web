c4785b6ce20e9529c8e778c598feb27eeafa6e69
fino87 <fino87@mail.ru>
2016-03-23 10:37:26 +0300

    ver 0.8

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,10 +1,6 @@
-from django.conf.urls import url
-urlpatterns=[
-	url(r'^$','qa.views.test',name='test'),
-	url(r'^login/$','qa.views.test',name='test'),
-	url(r'^signup/$','qa.views.test',name='test'),
-	url(r'^question/\d+/$','qa.views.test',name='test'),
-	url(r'^ask/','qa.views.test',name='test'),
-	url(r'^popular/$','qa.views.test',name='test'),
-	url(r'^new/$','qa.views.test',name='test')
-	]
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+        url(r'^', include('qa.urls')),
+]
