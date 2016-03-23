e4890dba1abd950d2ab82d208e4da800f48260d5
fino87 <fino87@mail.ru>
2016-03-23 22:48:46 +0300

    ver

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -75,10 +75,19 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db1',
+        'USER': 'django',
+        'PASSWORD': '123',
     }
 }
 
