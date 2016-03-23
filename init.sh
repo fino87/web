144fb97fe169d6036103671d4910a7eda29c804a
fino87 <fino87@mail.ru>
2016-03-23 13:35:45 +0300

    ver 0.9

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
+sudo /home/box/web/ask/manage.py syncdb
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
